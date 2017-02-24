ce09dd3f47d33ce57b8f2668d982041be3b77b55
Herman Lin <hlin9@stuy.edu>
2017-02-24 18:05:03 -0500

    hw08

--- CS/HW08/QueenBoard.java
+++ CS/HW08/QueenBoard.java
@@ -16,7 +16,7 @@ public class QueenBoard
     //constructor allows for specifying size
     public QueenBoard( int size ) 
     {
-	_board = new int[size][size];
+		_board = new int[size][size];
     }
 
 
@@ -32,7 +32,12 @@ public class QueenBoard
      */
     public boolean solve()
     {
-	return false;
+		if (solveH(0)) {
+			//System.out.println(this);
+			return true;
+		}
+		//System.out.println("no solution");
+		return false;
     }
 
     /**
@@ -40,7 +45,18 @@ public class QueenBoard
      */
     private boolean solveH( int col ) 
     {
-	return false;
+		if (col == _board.length)
+			return true;
+		for(int row = 0; row < _board.length; row ++) {
+			//System.out.println("Trying " + row + " " + col);
+			if (_board[row][col] == 0) {
+				addQueen(row, col);
+				if(solveH(col + 1))
+					return true;
+			}
+			removeQueen(row, col);
+		}
+		return false;
     }
 
 
@@ -52,6 +68,25 @@ public class QueenBoard
 	    all negs and 0's replaced with underscore
 	    all 1's replaced with 'Q'
 	*/
+		if (solve()) {
+			for (int x = 0; x < _board.length; x ++) {
+				for (int y = 0; y < _board.length; y ++) {
+					if (_board[x][y] == 1)
+						System.out.print("Q ");
+					else
+						System.out.print("_ ");
+				}
+				System.out.println();
+			}
+		}
+		else if(!solve()) {
+			for (int x = 0; x < _board.length; x ++) {
+				for (int y = 0; y < _board.length; y ++) {
+					System.out.print("0 ");
+				}
+				System.out.println();
+			}
+		}
     }
 
 
@@ -59,9 +94,10 @@ public class QueenBoard
     //================= YE OLDE SEPARATOR =================
 
     /***
-     * <General description>
-     * precondition:
-     * postcondition: 
+     * addQueen
+     * precondition: takes a row and a column
+     * postcondition: places a queen in [row, col] and replaces
+     * the invalid locations with a negative numberr
      */
     private boolean addQueen(int row, int col)
     {
@@ -76,19 +112,21 @@ public class QueenBoard
 	// counter to mark squares that a queen can attack
 	int offset = 1;
 
-	// ?
+	// while the col+offset is !> the row length
 	while(col+offset < _board[row].length) {
-	    // ?
+	    // subtract one from bottom right square
 	    _board[row][col+offset]--;
 
-	    // ?
+	    // subtract one from the bottom left square
 	    if( row - offset >= 0 ) {
 		_board[row-offset][col+offset]--;
 	    }
+	    // subtract one from the top right square
 	    if( row + offset < _board.length ) {
 		_board[row+offset][col+offset]--;
 	    }
-	    // ?
+	    // inccrease the offset to accomodate for the
+	    // next diagonal squares
 	    offset++;
 	}
 	return true;
@@ -96,20 +134,21 @@ public class QueenBoard
 
 
     /***
-     * <General description>
-     * precondition: 
-     * postcondition: 
+     * removeQueen
+     * precondition: takes a row and a column 
+     * postcondition: removes a queen from the square
      */
     private boolean removeQueen(int row, int col)
     {
-	// ?
+	// if there is no queen at [row, col]
 	if ( _board[row][col] != 1 ) {
 	    return false;
 	}
-	_board[row][col] = 0;	// ?
-	int offset = 1;	        // ?
+	_board[row][col] = 0;	// "remove" the queen from [row, col]
+	int offset = 1;	        // set offset to 1 so you can remove invalid locations
 
-	// ?
+	// while col+offset !> the row length, traverse the diagonals, removing the
+	// invalid locations as you move along
 	while( col+offset < _board[row].length ) {
 	    _board[row][col+offset]++;
 	    if( row - offset >= 0 ) {
@@ -125,9 +164,9 @@ public class QueenBoard
 
 
     /***
-     * <General description>
+     * toString
      * precondition: 
-     * postcondition: 
+     * postcondition: prints out the board with invalid locations as neg numbers 
      */
     public String toString() 
     {
@@ -145,13 +184,52 @@ public class QueenBoard
     //main method for testing...
     public static void main( String[] args )
     {
-	QueenBoard b = new QueenBoard(5);
-        System.out.println(b);
+	QueenBoard a = new QueenBoard(1);
+	QueenBoard b = new QueenBoard(2);
+	QueenBoard c = new QueenBoard(3);
+	QueenBoard d = new QueenBoard(4);
+	QueenBoard e = new QueenBoard(5);
+	QueenBoard f = new QueenBoard(6);
+	QueenBoard g = new QueenBoard(7);
+	QueenBoard h = new QueenBoard(8);
+	
+	//QueenBoard i = new QueenBoard(20);
+	/*
+    System.out.println(b);
 	b.addQueen(3,0);
 	b.addQueen(0,1);
-        System.out.println(b);
+    System.out.println(b);
 	b.removeQueen(3,0);
-        System.out.println(b);
+    System.out.println(b);
+		*/
+	System.out.println();
+	System.out.println("1x1 Board");
+	a.printSolution();
+	System.out.println();
+	System.out.println("2x2 Board");
+	b.printSolution();
+	System.out.println();
+	System.out.println("3x3 Board");
+	c.printSolution();
+	System.out.println();
+	System.out.println("4x4 Board");
+	d.printSolution();
+	System.out.println();
+	System.out.println("5x5 Board");
+	e.printSolution();
+	System.out.println();
+	System.out.println("6x6 Board");
+	f.printSolution();
+	System.out.println();
+	System.out.println("7x7 Board");
+	g.printSolution();
+	System.out.println();
+	System.out.println("8x8 Board");
+	h.printSolution();
+	
+	//System.out.println();
+	//System.out.println("20x20 Board");
+	//i.printSolution();
     }//end main()
     
 }//end class
