e74f50e82df35e945cd71df8f792e0e32d008f98
Herman Lin <hlin9@stuy.edu>
2017-03-22 14:49:40 -0400

    hw22

--- CS/HW22/Latkes.java
+++ CS/HW22/Latkes.java
@@ -1,6 +1,11 @@
+/*Herman Lin
+APCS2 - pd3
+HW22 -- Stacks on Stacks on Stacks...
+2017-3-22*/
+
+
 /***
  * class Latkes
- * SKELETON
  * (actually: a class to implement a stack of Strings)
  * uses an array as underlying data structure
  ***/
@@ -14,7 +19,8 @@ public class Latkes
     //constructor
     public Latkes(int size) 
     { 
-	/* YOUR IMPLEMENTATION HERE */
+	_stack = new String[size];
+	_stackSize = 0;
     }
 
     //means of insertion
@@ -32,13 +38,17 @@ public class Latkes
     //chk for emptiness
     public boolean isEmpty() 
     { 
-	/* YOUR IMPLEMENTATION HERE */
+	if (_stackSize == 0)
+	    return true;
+	return false;
     }
 
     //chk for fullness
     public boolean isFull() 
     {
-	/* YOUR IMPLEMENTATION HERE */
+	if (_stackSize == _stack.size)
+	    return true;
+	return false;
     }
 
 }//end class Latkes
